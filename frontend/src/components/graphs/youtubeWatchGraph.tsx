6b6d34d0b24ca535f85c39f3093f2ee9dee7c610
Kiduzk <70627866+Kiduzk@users.noreply.github.com>
2025-08-19 22:49:21 -0500

    fix: chart width

--- frontend/src/components/graphs/youtubeWatchGraph.tsx
+++ frontend/src/components/graphs/youtubeWatchGraph.tsx
@@ -15,23 +15,30 @@ export const YoutubeWatchGraph = ({
   youtubeWatchDataGraph: { date: string; count: number }[];
   darkMode: boolean;
 }) => (
-  <LineChart
-    width={600}
-    height={300}
-    data={youtubeWatchDataGraph.slice(1, 10)}
-    margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
-  >
-    <CartesianGrid stroke="#aaa" strokeDasharray="5 5" />
-    <Line
-      type="monotone"
-      dataKey="count"
-      stroke={darkMode ? "white" : "black"}
-      strokeWidth={2}
-      name="Videos Watched"
-    />
-    <XAxis dataKey="date" />
-    <YAxis width={80} />
-    <Legend align="right" />
-    <Tooltip contentStyle={{ backgroundColor: darkMode ? '#333' : '#fff', color: darkMode ? '#fff' : '#000' }} />
-  </LineChart>
+  <div>
+    <LineChart
+      width={1200}
+      height={300}
+      data={youtubeWatchDataGraph.slice(1, 100)}
+      margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+    >
+      <CartesianGrid stroke="#aaa" strokeDasharray="5 5" />
+      <Line
+        type="monotone"
+        dataKey="count"
+        stroke={darkMode ? "white" : "black"}
+        strokeWidth={2}
+        name="Videos Watched"
+      />
+      <XAxis dataKey="date" />
+      <YAxis width={80} />
+      <Legend align="right" />
+      <Tooltip
+        contentStyle={{
+          backgroundColor: darkMode ? "#333" : "#fff",
+          color: darkMode ? "#fff" : "#000",
+        }}
+      />
+    </LineChart>
+  </div>
 );
